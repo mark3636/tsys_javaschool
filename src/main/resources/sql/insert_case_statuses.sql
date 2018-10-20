2bebe81a477f279ad76ffac0f647b154bb6129fa
Mark Lukin <mark.lukin36@gmail.com>
2018-10-20 16:30:27 +0300

    added case_status insert script

---
+++ src/main/resources/sql/insert_case_statuses.sql
@@ -0,0 +1,13 @@
+USE Medical_institute;
+
+INSERT INTO case_status(name, description)
+VALUES('OPENED', 'Case is opened');
+
+INSERT INTO case_status(name, description)
+VALUES('CLOSED', 'Case is closed');
+
+INSERT INTO case_status(name, description)
+VALUES('CANCELLED', 'Case is cancelled');
+
+INSERT INTO case_status(name, description)
+VALUES('COMPLETED', 'Case is completed');
