29d09e7e3c314bcae76bd2d00b88936c9f804c10
Mark Lukin <mark.lukin36@gmail.com>
2018-10-22 20:32:48 +0300

    Configuration updated

--- src/main/java/ru/tsystems/medicalinstitute/config/WebMvcConfig.java
+++ src/main/java/ru/tsystems/medicalinstitute/config/WebMvcConfig.java
@@ -3,6 +3,8 @@ package ru.tsystems.medicalinstitute.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
+import org.springframework.web.multipart.commons.CommonsMultipartResolver;
 import org.springframework.web.servlet.ViewResolver;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
@@ -11,21 +13,28 @@ import org.springframework.web.servlet.view.JstlView;
 
 @EnableWebMvc
 @Configuration
-@ComponentScan({"ru.tsystems.medicalinstitute.controller"})
+@Import({WebSecurityConfig.class})
+@ComponentScan({"ru.tsystems.medicalinstitute"})
 public class WebMvcConfig {
     public void addViewControllers(ViewControllerRegistry registry) {
         registry.addViewController("/");
-        registry.addViewController("/login").setViewName("login");
+        registry.addViewController("/login");
     }
 
     @Bean
     public ViewResolver viewResolver() {
         InternalResourceViewResolver resolver = new InternalResourceViewResolver();
-
         resolver.setViewClass(JstlView.class);
         resolver.setPrefix("/views/");
         resolver.setSuffix(".jsp");
+        return resolver;
+    }
 
+    @Bean
+    public CommonsMultipartResolver multipartResolver() {
+        CommonsMultipartResolver resolver = new CommonsMultipartResolver();
+        resolver.setMaxUploadSize(10000000);
+        resolver.setDefaultEncoding("utf-8");
         return resolver;
     }
 }
--- src/main/java/ru/tsystems/medicalinstitute/config/WebSecurityConfig.java
+++ src/main/java/ru/tsystems/medicalinstitute/config/WebSecurityConfig.java
@@ -1,45 +1,57 @@
 package ru.tsystems.medicalinstitute.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
-import org.springframework.security.config.annotation.authentication.configuration.GlobalAuthenticationConfigurerAdapter;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import ru.tsystems.medicalinstitute.bo.MedicalStaff;
+import ru.tsystems.medicalinstitute.service.MedicalStaffService;
+
+import java.util.Date;
 
 @Configuration
 @EnableWebSecurity
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+    @Autowired
+    MedicalStaffService medicalStaffService;
+
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http
+        http.csrf().disable()
                 .authorizeRequests()
-                .antMatchers("/", "/home").permitAll()
-                .anyRequest().authenticated()
-                .and()
-                .formLogin()
-                .loginPage("/login")
-                .permitAll()
-                .and()
-                .logout()
-                .permitAll();
+                .antMatchers("/**").permitAll();
+//                .antMatchers("/**").access("hasRole('ROLE_USER')")
+//                .and()
+//                .formLogin()
+//                .loginPage("/login")
+//                .usernameParameter("email")
+//                .passwordParameter("password")
+//                .and().csrf()
+//                .and().exceptionHandling();
+    }
+
+    @Autowired
+    public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
+        auth.userDetailsService(medicalStaffService)
+                .passwordEncoder(passwordEncoder());
     }
 
     @Bean
-    @Override
-    public UserDetailsService userDetailsService() {
-        UserDetails user =
-                User.withDefaultPasswordEncoder()
-                        .username("user")
-                        .password("password")
-                        .roles("USER")
-                        .build();
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
 
-        return new InMemoryUserDetailsManager(user);
+    @Bean
+    public DaoAuthenticationProvider authenticationProvider() {
+        DaoAuthenticationProvider authenticationProvider = new DaoAuthenticationProvider();
+        authenticationProvider.setUserDetailsService(medicalStaffService);
+        authenticationProvider.setPasswordEncoder(passwordEncoder());
+        return authenticationProvider;
     }
 }
