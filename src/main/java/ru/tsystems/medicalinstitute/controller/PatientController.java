0a3279b16e9a44d5d3004eb9327335d6b972ea53
Mark Lukin <mark.lukin36@gmail.com>
2018-11-28 18:56:58 +0300

    Some handlers moved to VisitController

--- src/main/java/ru/tsystems/medicalinstitute/controller/PatientController.java
+++ src/main/java/ru/tsystems/medicalinstitute/controller/PatientController.java
@@ -7,29 +7,24 @@ import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
 import ru.tsystems.medicalinstitute.bo.Patient;
-import ru.tsystems.medicalinstitute.bo.Visit;
 import ru.tsystems.medicalinstitute.service.MedicalCaseService;
-import ru.tsystems.medicalinstitute.service.MedicalStaffService;
 import ru.tsystems.medicalinstitute.service.PatientService;
 import ru.tsystems.medicalinstitute.service.VisitService;
 
 import javax.validation.Valid;
 import java.text.ParseException;
-import java.util.List;
 
 @Controller
 public class PatientController {
     private final PatientService patientService;
     private final MedicalCaseService medicalCaseService;
     private final VisitService visitService;
-    private final MedicalStaffService medicalStaffService;
 
     public PatientController(final PatientService patientService, final MedicalCaseService medicalCaseService,
-                             final VisitService visitService, final MedicalStaffService medicalStaffService) {
+                             final VisitService visitService) {
         this.patientService = patientService;
         this.medicalCaseService = medicalCaseService;
         this.visitService = visitService;
-        this.medicalStaffService = medicalStaffService;
     }
 
     @RequestMapping(value = "/", method = RequestMethod.GET)
@@ -67,43 +62,6 @@ public class PatientController {
         model.addAttribute("patientStatus", patientService.getPatientStatus(id));
 
         return "patient-details";
-    }
-
-    @RequestMapping(value = "/patient-details/{patientId}/new-visit", method = RequestMethod.GET)
-    public String newVisit(@PathVariable("patientId") int id, Model model) {
-        model.addAttribute("patient", patientService.getById(id));
-        model.addAttribute("visits", visitService.listVisits());
-        model.addAttribute("medicalStaff", medicalStaffService.listMedicalStaff());
-        model.addAttribute("medicalStaffId", "");
-
-        return "visit";
-    }
-
-    @RequestMapping(value = "/patient-details/{patientId}/new-visit-beginning-time", method = RequestMethod.GET)
-    public @ResponseBody
-    List<String> getExistedTime(
-            @RequestParam(value = "medicalStaff", required = false) String medicalStaff,
-            @RequestParam(value = "visitDate", required = false) String visitDate) throws ParseException {
-        return visitService.getExistedTime(Integer.parseInt(medicalStaff), visitDate);
-    }
-
-    @RequestMapping(value = "/patient-details/{patientId}/new-visit-ending-time", method = RequestMethod.GET)
-    public @ResponseBody
-    List<String> getEndingTime(
-            @RequestParam(value = "medicalStaff", required = false) String medicalStaff,
-            @RequestParam(value = "visitDate", required = false) String visitDate,
-            @RequestParam(value = "beginningTime", required = false) String beginningTime) throws ParseException {
-        return visitService.getEndingTime(Integer.parseInt(medicalStaff), visitDate, beginningTime);
-    }
-
-    @RequestMapping(value = "/patient-details/{patientId}/new-visit", method = RequestMethod.POST)
-    public String newVisit(@ModelAttribute("visit") Visit visit, @PathVariable("patientId") int patientId,
-                           @ModelAttribute("medicalStaffId") int medicalStaffId) {
-        visit.setMedicalStaff(medicalStaffService.getById(medicalStaffId));
-        visit.setPatient(patientService.getById(patientId));
-        visitService.add(visit);
-
-        return "redirect:/patient-details/{patientId}";
     }
 
     @RequestMapping(value = "/patient", method = RequestMethod.POST)
