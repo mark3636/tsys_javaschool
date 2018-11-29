6a7235d4bae96b0f3c7dba7bf51a5bff35085f5a
Mark Lukin <mark.lukin36@gmail.com>
2018-11-29 18:38:36 +0300

    Registration controller updated

--- src/main/java/ru/tsystems/medicalinstitute/controller/LoginController.java
+++ src/main/java/ru/tsystems/medicalinstitute/controller/LoginController.java
@@ -1,6 +1,8 @@
 package ru.tsystems.medicalinstitute.controller;
 
-import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
@@ -9,16 +11,22 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import ru.tsystems.medicalinstitute.bo.MedicalStaff;
+import ru.tsystems.medicalinstitute.bo.Role;
 import ru.tsystems.medicalinstitute.service.MedicalStaffService;
+import ru.tsystems.medicalinstitute.service.RoleService;
 
 import javax.validation.Valid;
+import java.util.Collection;
 
 @Controller
 public class LoginController {
     private final MedicalStaffService medicalStaffService;
+    private final RoleService roleService;
 
-    public LoginController(final MedicalStaffService medicalStaffService) {
+    public LoginController(final MedicalStaffService medicalStaffService,
+                           final RoleService roleService) {
         this.medicalStaffService = medicalStaffService;
+        this.roleService = roleService;
     }
 
     @RequestMapping(value = "/login", method = RequestMethod.GET)
@@ -33,6 +41,8 @@ public class LoginController {
     public String showRegistrationForm(@RequestParam(value = "error", required = false) String error, Model model) {
         MedicalStaff medicalStaff = new MedicalStaff();
         model.addAttribute("medicalStaff", medicalStaff);
+        model.addAttribute("roles", roleService.listRoles());
+        model.addAttribute("roleId", "");
 
         if (error != null) {
             model.addAttribute("error", "User with such email already exists");
@@ -43,9 +53,11 @@ public class LoginController {
 
     @RequestMapping(value = "/registration", method = RequestMethod.POST)
     public String register(
-            @ModelAttribute("medicalStaff") @Valid MedicalStaff medicalStaff,
+            @ModelAttribute("medicalStaff") MedicalStaff medicalStaff,
+            @ModelAttribute("roleId") int roleId,
             BindingResult result, Model model) {
         if (result.hasErrors()) {
+            medicalStaff.setPassword(null);
             return "registration";
         }
 
@@ -55,8 +67,22 @@ public class LoginController {
             return "registration";
         }
 
+        medicalStaff.setRole(roleService.getById(roleId));
         medicalStaffService.add(medicalStaff);
 
         return "login";
+    }
+
+    @RequestMapping(value = "/", method = RequestMethod.GET)
+    public String homePage() {
+        Collection<GrantedAuthority> authorities = (Collection<GrantedAuthority>) SecurityContextHolder.getContext().getAuthentication().getAuthorities();
+
+        for (GrantedAuthority auth : authorities) {
+            if (auth.getAuthority().equals("ROLE_NURSE")) {
+                return "redirect:/medical-procedures";
+            }
+        }
+
+        return "redirect:/patients";
     }
 }
