2b95492641df5b18f9216afe78d46edc6b85d09b
Mark Lukin <mark.lukin36@gmail.com>
2018-11-29 18:40:25 +0300

    Bo for procedure comment added

---
+++ src/main/java/ru/tsystems/medicalinstitute/bo/ProcedureCommentForm.java
@@ -0,0 +1,31 @@
+package ru.tsystems.medicalinstitute.bo;
+
+public class ProcedureCommentForm {
+    private String status;
+    private int id;
+    private String comment;
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getComment() {
+        return comment;
+    }
+
+    public void setComment(String comment) {
+        this.comment = comment;
+    }
+}
